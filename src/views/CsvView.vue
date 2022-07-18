1b582c74fb876ddd9525d72608c9221583012b23
Xavier <dejuan.xavier@gmail.com>
2022-07-18 13:20:09 +0200

    wip

--- src/views/CsvView.vue
+++ src/views/CsvView.vue
@@ -56,39 +56,41 @@ export default {
   }),
   methods: {
 
-     async sendCsvList(){
-        try{
-            console.log('[Views][CsvView][sendCsvList] Called sendCsvList with params', )
-            
-            //todo use papaparse to convert from csv to json
-            //todo send to the service the json produced
-            const response = await CsvService.sendJsonList();
+    async sendCsvList() {
+      try {
+        console.log('[Views][CsvView][sendCsvList] Called sendCsvList with params',)
 
-            // set the blog type to final pdf
-            const file = new Blob([response.data], {type: 'application/pdf'});
+        //todo use papaparse to convert from csv to json
+        //todo send to the service the json produced
+        const response = await CsvService.sendJsonList();
 
-            // process to auto download it
-            const fileURL = URL.createObjectURL(file);
-            const link = document.createElement('a');
-            link.href = fileURL;
-            link.download = "FileName" + new Date().getTime() + ".pdf";
-            link.click();
+        // set the blog type to final pdf
+        const file = new Blob([response.data], { type: 'application/pdf' });
 
-        }catch(e){
-            console.error('[Views][CsvView][sendCsvList] An error has occurred when send the csv list', e)
-            //todo handle the error
-        }
-    }
-  },
-  papaparse(convert) {
-    this.$papa.parse(convert, {
-      header: true,
-      download: true,
-      complete: function (result) {
-        this.datas = result;
-        console.log(this.datas);
+        // process to auto download it
+        const fileURL = URL.createObjectURL(file);
+        const link = document.createElement('a');
+        link.href = fileURL;
+        link.download = "FileName" + new Date().getTime() + ".pdf";
+        link.click();
+
+      } catch (e) {
+        console.error('[Views][CsvView][sendCsvList] An error has occurred when send the csv list', e)
+        //todo handle the error
       }
-    })
-  },
+    },
+
+    papaparse(convert) {
+      this.$papa.parse(convert, {
+        header: true,
+        download: true,
+        complete: function (result) {
+          console.log(result)
+          // this.datas = result;
+          // console.log(this.datas);
+        }
+      })
+    },
+  }
 }
 </script>
