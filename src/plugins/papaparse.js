9f5b32132fd66715d7beb0843caa5adb01c8684c
baouad <83958965+baouad@users.noreply.github.com>
2022-07-27 15:22:22 +0200

    Wip FORM
    Form completed with validation and Json object

--- src/plugins/papaparse.js
+++ src/plugins/papaparse.js
@@ -1,4 +1,4 @@
-import Vue from 'vue';
-import VuePapaParse from 'vue-papa-parse'
-Vue.use(VuePapaParse)
+// import Vue from 'vue';
+// import VuePapaParse from 'vue-papa-parse'
+// Vue.use(VuePapaParse)
 
