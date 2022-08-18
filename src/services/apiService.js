6b6eb80cf921d95c676e465c1761aa40c802e2d0
Marco Tribuzio <marco.tribuz@gmail.com>
2022-08-18 14:04:11 +0200

    WIP

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL, API_KEY, CSVLIST_OPTIONS } from "@/libs/consts";
+import {API_KEY, BASE_URL, CSVLIST_OPTIONS} from "@/libs/consts";
 
 /**
  * @class
@@ -7,117 +7,93 @@ import { BASE_URL, API_KEY, CSVLIST_OPTIONS } from "@/libs/consts";
  */
 class ApiService {
 
-    /**
-     * Service to send the list to the api, and receive the pdf list to download
-     * @async
-     * @param {object[]} csvList - the list that we want to send
-     * @return Promise<Object>
-     */
-    static async sendJsonList(csvList) {
-        try {
+  /**
+   * Service to send the list to the api, and receive the pdf list to download
+   * @async
+   * @param {object[]} csvList - the list that we want to send
+   * @return Promise<Object>
+   */
+  static async sendJsonList(csvList) {
+    try {
 
-            const payload = null
+      const payload = null
 
-            const data = {
-                "creditorInformation": {
-                    "iban": "CH",
-                    "creditor": {
-                        "addressType": "STRUCTURED",
-                        "name": "Réalise S.A.",
-                        "streetName": "Rue Viguet",
-                        "houseNumber": "8",
-                        "postalCode": "1227",
-                        "city": "Genève",
-                        "addressLine1": "Rue Viguet 8 1227 les Acacias",
-                        "addressLine2": "1227 Les acacias genève",
-                        "country": "CH"
-                    }
-                },
-                "ultimateCreditor": {
-                    "addressType": "STRUCTURED",
-                    "name": "Réalise Genève",
-                    "streetName": "Rue Viguet",
-                    "houseNumber": "8",
-                    "postalCode": "1227",
-                    "city": "Genève",
-                    "addressLine1": "Rue Viguet 8 1227 les Acacias",
-                    "addressLine2": "1227 Les acacias genève",
-                    "country": "CH"
-                },
-                "paymentAmountInformation": {
-                    "amount": "",
-                    "currency": "CHF"
-                },
-                 
-                "ultimateDebtor": {
-                    "addressType": "STRUCTURED",
-                    "name":"",
-                    "streetName": "",
-                    "houseNumber": "",
-                    "postalCode": "",
-                    "city": "",
-                    "addressLine1": "",
-                    "addressLine2": "",
-                    "country": "CH"
-                },
-                "paymentReference": {
-                    "referenceType": "SCOR",
-                    "reference": "",
-                    "additionalInformation": {
-                        "unstructuredMessage": "Bill No. 3139 for garden work and disposal of cuttings",
-                        "billInformation": "//S1/10/10201409/11/190512/20/1400.000-53/30/106017086/31/180508/32/7.7/40/2:10;0:30",
-                        "billInformationObject": {}
-                    }
-                },
-                "alternativeSchemes": {
-                    "alternativeSchemeParameters": [
-                        "Name AV1: UV;UltraPay005;12345"
-                    ]
-                },
-            }  
-            console.log("payload", payload)           
-            console.log('[Service][CsvService][sendCsvList] Called sendCsvList with params', csvList, data)
-            console.log(BASE_URL + '/v2/payment-part-receipt' + API_KEY + '&' + CSVLIST_OPTIONS)
-            const response = await axios.post(BASE_URL + '/v2/payment-part-receipt' + API_KEY + '&' + CSVLIST_OPTIONS, data,
-                {
-                    responseType: "blob",
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'accept': 'application/pdf',
-                        'Accept-Language': 'fr'
-                    }
-                },
-            )
-            return response
-        } catch (e) {
-            console.error('[Service][CsvService][sendJsonList] An error has occurred when sending the list to the api', e)
-            throw new Error(e)
-        }
+      const data = {
+        "creditorInformation": {
+          "iban": "CH4431999123000889012",
+          "creditor": {
+            "addressType": "STRUCTURED",
+            "name": "Robert Schneider AG",
+            "streetName": "Rue du Lac",
+            "houseNumber": "1268",
+            "postalCode": "2501",
+            "city": "Biel",
+            "country": "CH"
+          }
+        },
+        "paymentAmountInformation": {"amount": 1949.75, "currency": "CHF"},
+        "ultimateDebtor": {
+          "addressType": "STRUCTURED",
+          "name": "Pia-Maria Rutschmann-Schnyder",
+          "streetName": "Grosse Marktgasse",
+          "houseNumber": "28",
+          "postalCode": "9400",
+          "city": "Rorschach",
+          "country": "CH"
+        },
+        "paymentReference": {
+          "referenceType": "QRR",
+          "reference": "210000000003139471430009017",
+          "additionalInformation": {
+            "unstructuredMessage": "Instruction of 03.04.2019",
+            "billInformation": "//S1/10/10201409/11/190512/20/1400.000-53/30/106017086/31/180508/32/7.7/40/2:10;0:30"
+          }
+        },
+        "alternativeSchemes": {"alternativeSchemeParameters": ["Name AV1: UV;UltraPay005;12345", "Name AV2: XY;XYService;54321"]}
+      }
+      console.log("payload", payload)
+      console.log('[Service][CsvService][sendCsvList] Called sendCsvList with params', csvList, data)
+      console.log(BASE_URL + '/v2/payment-part-receipt' + API_KEY + '&' + CSVLIST_OPTIONS)
+      const response = await axios.post(BASE_URL + '/v2/payment-part-receipt' + API_KEY + '&' + CSVLIST_OPTIONS, data,
+        {
+          responseType: "blob",
+          headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/pdf',
+            'Accept-Language': 'fr'
+          }
+        },
+      )
+      return response
+    } catch (e) {
+      console.error('[Service][CsvService][sendJsonList] An error has occurred when sending the list to the api', e)
+      throw new Error(e)
     }
+  }
 
-    /**
-     * Sending Single Payment to the api
-     * @param payload
-     * @returns {Promise<*>}
-     */
-    static async sendSinglePayment(payload) {
-        try {
-            const response = await axios.post(BASE_URL + '/v2/payment-part-receipt' + API_KEY + '&' + CSVLIST_OPTIONS, payload,
-                {
-                    responseType: "blob",
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'accept': 'application/pdf',
-                        'Accept-Language': 'fr'
-                    }
-                },
-            )
-            return response
-        }catch (e) {
-            console.error('[Service][CsvService][sendSinglePayment] An error has occurred when sending a single payment to the api', e)
-            throw new Error(e)
-        }
+  /**
+   * Sending Single Payment to the api
+   * @param payload
+   * @returns {Promise<*>}
+   */
+  static async sendSinglePayment(payload) {
+    try {
+      const response = await axios.post(BASE_URL + '/v2/payment-part-receipt' + API_KEY + '&' + CSVLIST_OPTIONS, payload,
+        {
+          responseType: "blob",
+          headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/pdf',
+            'Accept-Language': 'fr'
+          }
+        },
+      )
+      return response
+    } catch (e) {
+      console.error('[Service][CsvService][sendSinglePayment] An error has occurred when sending a single payment to the api', e)
+      throw new Error(e)
     }
+  }
 }
 
 export default ApiService
