ba1f7eae9a9de6773bb29ebe790a3e07caf7b076
Xavier <dejuan.xavier@gmail.com>
2022-09-14 09:43:06 +0200

    WIP
    Nouveau départ

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -1,18 +1,11 @@
 <template>
     <v-row>
-        <textarea v-model="CommentsMkDwn"></textarea>
-        {{ CommentsMkDwn}}
     </v-row>
 </template>
 <script>
-export default {
-    name: "Info-View",
-    data() {
-        return {
-            CommentsMkDwn: "Voilàààààà!!!!! 'Return' pour affichage de résultat c'est pas compliqué non?"
-        }
+    export default {
+        name:"Info-View",
     }
-};
 </script>
 <style>
 </style>
