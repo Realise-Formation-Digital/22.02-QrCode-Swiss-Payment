2f8a59de790f178df2b63108377adaa1ee39eb2e
Xavier <dejuan.xavier@gmail.com>
2022-09-16 15:51:20 +0200

    WIP
    Ajout la possibilité d'écrire dans "rue":
    éàèöäü/_

--- src/views/FormQrView.vue
+++ src/views/FormQrView.vue
@@ -609,7 +609,7 @@ export default {
      */
     lettreSeulement(e) {
       let char = String.fromCharCode(e.keyCode); // Get the character
-      if (/^[A-Za-z- ---.-]+$/.test(char)) return true; // Match with regex 
+      if (/^[A-Za-z-èéàüöä ---.-/-_-]+$/.test(char)) return true; // Match with regex 
       else e.preventDefault(); // If not match, don't add to input text
     },
 
