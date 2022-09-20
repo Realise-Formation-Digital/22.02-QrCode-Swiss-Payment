08562a571712a3b84d8bf760455d211c9aa2689e
Marco Tribuzio <marco.tribuz@gmail.com>
2022-09-20 16:08:21 +0200

    Update ENV Variable

--- src/libs/consts.js
+++ src/libs/consts.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://rest.qr-invoice.cloud';
+const BASE_URL = process.env.BASE_URL;
 const API_KEY = process.env.VUE_APP_APIKEY;
-const CSVLIST_OPTIONS = 'fontFamily=LIBERATION_SANS&fontsEmbedded=true&pageSize=DIN_LANG&resolution=MEDIUM_300_DPI&boundaryLines=true&boundaryLinesMargins=false&boundaryLineScissors=true&boundaryLineSeparationText=false&additionalPrintMargin=false&normalizeInput=false';
-const MARKDWN_URL ="http://localhost:8080/README.md"
+const CSVLIST_OPTIONS = process.env.VUE_APP_CSVLIST_OPTIONS;
+const MARKDWN_URL = process.env.VUE_APP_MARKDOWN_URL;
 export {
   BASE_URL,
   API_KEY,
