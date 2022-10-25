2479aa2acd19de051397b3bce34dd0d10ff92eee
Xavier <dejuan.xavier@gmail.com>
2022-10-25 09:08:33 +0200

    WIP Add lib

---
+++ src/libs/pdf.js
@@ -0,0 +1,13 @@
+import { PDFDocument } from 'pdf-lib';
+
+class Pdf {
+
+  static async test(file){
+    const pdfDoc = await PDFDocument.load(file)
+    console.log("ciao", file)
+
+  }
+
+}
+
+export default Pdf
